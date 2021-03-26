ae56261e9f35a77fe0bb433616940a4355ad37cb
nunopenim <nuno.penim@gmail.com>
2021-03-26 14:28:37 +0000

    general: introduced Vacinas class

---
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/blocs/Vacinas.kt
@@ -0,0 +1,7 @@
+package pt.ulusofona.deisi.a2020.cm.g3.blocs
+
+class Vacinas (var data: String, var doses: Int?, var doses_novas: Int?,
+               var doses1: Int?, var doses1_novas: Int?, var doses2: Int?, var doses2_novas: Int?) {
+
+    //falta métodos
+}

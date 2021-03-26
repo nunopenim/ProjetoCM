2352fe1889f928f6f826eb0c70bef284e8c4b8a5
nunopenim <nuno.penim@gmail.com>
2021-03-26 14:36:17 +0000

    Vacinas: moved to a new package, dedicated to external data

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/blocs/API/Vacinas.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/blocs/API/Vacinas.kt
@@ -1,4 +1,4 @@
-package pt.ulusofona.deisi.a2020.cm.g3.blocs
+package pt.ulusofona.deisi.a2020.cm.g3.blocs.API
 
 class Vacinas (var data: String, var doses: Int?, var doses_novas: Int?,
                var doses1: Int?, var doses1_novas: Int?, var doses2: Int?, var doses2_novas: Int?) {

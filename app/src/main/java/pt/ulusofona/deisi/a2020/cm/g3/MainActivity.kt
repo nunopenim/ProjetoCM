08bea479a06511bcb67e20375d40a11fd5b45b16
nunopenim <nuno.penim@gmail.com>
2021-03-29 14:18:34 +0100

    MainActivity: added toggles and listeners for the new hamburger menu

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/MainActivity.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/MainActivity.kt
@@ -2,10 +2,29 @@ package pt.ulusofona.deisi.a2020.cm.g3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navigationView: NavigationView
+    lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+        toolbar = findViewById(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+
+        var toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.close_burger, R.string.open_burger)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
     }
 }

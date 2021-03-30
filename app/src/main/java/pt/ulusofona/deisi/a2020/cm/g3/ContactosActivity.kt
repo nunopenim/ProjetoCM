a729230a478a8fc996b2af25770c7c00763654d1
nunopenim <nuno.penim@gmail.com>
2021-03-30 10:29:38 +0100

    general: added kt file for contactos

---
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/ContactosActivity.kt
@@ -0,0 +1,12 @@
+package pt.ulusofona.deisi.a2020.cm.g3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ContactosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contactos)
+    }
+}

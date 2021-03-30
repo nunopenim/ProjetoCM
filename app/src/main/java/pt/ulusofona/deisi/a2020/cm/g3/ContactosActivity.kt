a44976fbdfdb472e15d8eb61b5e12a203f436460
nunopenim <nuno.penim@gmail.com>
2021-03-30 10:51:24 +0100

    general: Added ContactosActivity

---
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/ContactosActivity.kt
@@ -0,0 +1,11 @@
+package pt.ulusofona.deisi.a2020.cm.g3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contactos)
+    }
+}

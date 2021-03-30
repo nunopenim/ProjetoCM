352769caefce0feff4f06ee88a96e6fa9f933236
nunopenim <nuno.penim@gmail.com>
2021-03-30 10:51:37 +0100

    general: Added ListaTestesActivity

---
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/ListaTestesActivity.kt
@@ -0,0 +1,11 @@
+package pt.ulusofona.deisi.a2020.cm.g3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaTestesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_testes)
+    }
+}

a81845462e7367c2077a1be41a5180570a6cd1d1
nunopenim <nuno.penim@gmail.com>
2021-04-06 09:47:03 +0100

    Teste: added photo parameter

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/blocs/Teste.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/blocs/Teste.kt
@@ -3,7 +3,7 @@ package pt.ulusofona.deisi.a2020.cm.g3.blocs
 import java.text.SimpleDateFormat
 import java.util.*
 
-class Teste (var local: String, var positivo: Boolean, var data: Date) {
+class Teste (var local: String, var positivo: Boolean, var data: Date, var photo: String?) {
 
     fun isOlder(t2: Teste): Boolean {
         if(this.data < t2.data) {
@@ -16,6 +16,13 @@ class Teste (var local: String, var positivo: Boolean, var data: Date) {
         val formatter = SimpleDateFormat("dd/MM/yyyy")
         val str_date = formatter.format(data)
         return str_date
+    }
+
+    fun hasPhoto() : Boolean {
+        if (photo == null || photo == "") {
+            return false
+        }
+        return true
     }
 
 }

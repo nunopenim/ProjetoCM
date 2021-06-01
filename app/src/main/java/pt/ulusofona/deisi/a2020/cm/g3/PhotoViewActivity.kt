513047e2200b6d2e6eb6ec43ba3eddc3eaaffe4c
nunopenim <nuno.penim@gmail.com>
2021-06-01 13:11:38 +0100

    PhotoViewActivity: fixed the photo not centered

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/PhotoViewActivity.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/PhotoViewActivity.kt
@@ -2,11 +2,6 @@ package pt.ulusofona.deisi.a2020.cm.g3
 
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.ScaleGestureDetector
-import android.view.ScaleGestureDetector.SimpleOnScaleGestureListener
-import android.widget.ImageView
-import android.widget.ScrollView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.github.chrisbanes.photoview.PhotoView
@@ -15,7 +10,6 @@ import com.github.chrisbanes.photoview.PhotoView
 class PhotoViewActivity : AppCompatActivity() {
     lateinit var toolbar: Toolbar
     lateinit var photo: PhotoView
-    lateinit var scrollView: ScrollView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +19,6 @@ class PhotoViewActivity : AppCompatActivity() {
 
         toolbar = findViewById(R.id.toolbar_photoview)
         photo = findViewById(R.id.teste_foto_view)
-        scrollView = findViewById(R.id.scrview)
         setSupportActionBar(toolbar)
         toolbar.setNavigationOnClickListener {onBackPressed()}
         val foto_pic = BitmapFactory.decodeByteArray(foto_id, 0, foto_id!!.size)

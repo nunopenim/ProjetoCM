945c55bb1ec00330a433d904832b28fb761b9b4a
nunopenim <nuno.penim@gmail.com>
2021-06-02 11:24:17 +0100

    NavigationManager: added for fragment navigation

---
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/NavigationManager.kt
@@ -0,0 +1,18 @@
+package pt.ulusofona.deisi.a2020.cm.g3
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+
+abstract class NavigationManager {
+    companion object {
+        private fun placeFragment(fm: FragmentManager, fragment: Fragment) {
+            val transition = fm.beginTransaction()
+            transition.replace(R.id.frame, fragment)
+            transition.addToBackStack(null)
+            transition.commit()
+        }
+        fun goToDashboard(fm: FragmentManager) {
+            placeFragment(fm, DashboardFragment())
+        }
+    }
+}

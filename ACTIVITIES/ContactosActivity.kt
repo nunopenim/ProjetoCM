e9d39fb0974f293709ccfc7e1133941035414b25
nunopenim <nuno.penim@gmail.com>
2021-06-02 11:21:54 +0100

    ContactosActivity: moved back after fixing testAdapter

--- ACTIVITIES/ContactosActivity.kt
+++ ACTIVITIES/ContactosActivity.kt
@@ -48,13 +48,13 @@ class ContactosActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
                 item.isChecked = false
             }
             R.id.vacinas -> {
-                startActivity(Intent(this, VacinacaoActivity::class.java))
+                //startActivity(Intent(this, VacinacaoActivity::class.java))
             }
             R.id.lista_testes -> {
-                startActivity(Intent(this, ListaTestesActivity::class.java))
+                //startActivity(Intent(this, ListaTestesActivity::class.java))
             }
             R.id.add_teste -> {
-                startActivity(Intent(this, RegistoTesteActivity::class.java))
+                //startActivity(Intent(this, RegistoTesteActivity::class.java))
             }
         }
         return true

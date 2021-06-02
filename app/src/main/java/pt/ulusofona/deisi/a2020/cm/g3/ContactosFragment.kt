e723f80835923e4d7f10343887f8b5f943d08099
nunopenim <nuno.penim@gmail.com>
2021-06-02 14:07:40 +0100

    general: added a fragment for contactos

---
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/ContactosFragment.kt
@@ -0,0 +1,15 @@
+package pt.ulusofona.deisi.a2020.cm.g3
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ContactosFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_contactos, container, false)
+    }
+
+}

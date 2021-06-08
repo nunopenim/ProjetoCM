2b3b25fd33a7cba44a39f0fa6d58afbf73b44850
nunopenim <nuno.penim@gmail.com>
2021-06-08 17:09:12 +0100

    MainActivity: cleanup of unused imports

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/MainActivity.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/MainActivity.kt
@@ -3,23 +3,12 @@ package pt.ulusofona.deisi.a2020.cm.g3
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import pt.ulusofona.deisi.a2020.cm.g3.remote.RetrofitBuilder
-import pt.ulusofona.deisi.a2020.cm.g3.remote.services.TodayEntry
-import pt.ulusofona.deisi.a2020.cm.g3.remote.services.VaccineEntry
-import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.util.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -31,7 +20,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT; // bloqueado na rotação because yes
-
         NavigationManager.goToDashboard(supportFragmentManager)
         drawerLayout = findViewById(R.id.drawer_layout)
         navigationView = findViewById(R.id.nav_view)
@@ -42,14 +30,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         navigationView.setNavigationItemSelectedListener(this)
-
-        /*
-        val service = RetrofitBuilder.getInstace(VacinasURL).create(VaccineEntry::class.java)
-        CoroutineScope(Dispatchers.IO).launch {
-            val response = service.fetchLatest()
-            Log.i("Confirmados", response.body()!![response.body()!!.size - 1].Data.toString())
-        }*/
-
     }
 
     override fun onBackPressed() {

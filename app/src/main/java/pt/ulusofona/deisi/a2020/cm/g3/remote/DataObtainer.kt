ecaed42984b60c182d106d1d0e6b684ed4f8338d
nunopenim <nuno.penim@gmail.com>
2021-06-08 13:40:14 +0100

    DataObtainer: adjustments

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/remote/DataObtainer.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/remote/DataObtainer.kt
@@ -1,6 +1,5 @@
 package pt.ulusofona.deisi.a2020.cm.g3.remote
 
-import android.util.Log
 import pt.ulusofona.deisi.a2020.cm.g3.blocs.API.Data
 import pt.ulusofona.deisi.a2020.cm.g3.blocs.API.Vacinas
 import pt.ulusofona.deisi.a2020.cm.g3.remote.services.TodayEntry
@@ -14,14 +13,19 @@ class DataObtainer {
         suspend fun getVaccines() : Vacinas {
             val service = RetrofitBuilder.getInstace(VacinasURL).create(VaccineEntry::class.java)
             val response = service.fetchLatest()
-            return response.body()!![response.body()!!.size - 1].toVacinasObject()
+            if (response.isSuccessful) {
+                return response.body()!![response.body()!!.size - 1].toVacinasObject()
+            }
+            return Vacinas("", 0, 0,0,0,0,0)
         }
 
         suspend fun getData() : Data{
             val service = RetrofitBuilder.getInstace(API_URL).create(TodayEntry::class.java)
             val response = service.fetchLatest()
-            //return Data("", "", 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
-            return response.body()!!.toDataObject()
+            if(response.isSuccessful) {
+                return response.body()!!.toDataObject()
+            }
+            return Data("", "", 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
         }
     }
 }

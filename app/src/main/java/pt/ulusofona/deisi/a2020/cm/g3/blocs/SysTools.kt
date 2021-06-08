17a1e3befe5c52bc4dac7a4d00929ac946cc3d3b
nunopenim <nuno.penim@gmail.com>
2021-06-08 17:08:31 +0100

    SysTools: we generate a date, might be useful

---
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/blocs/SysTools.kt
@@ -0,0 +1,17 @@
+package pt.ulusofona.deisi.a2020.cm.g3.blocs
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+class SysTools {
+    companion object {
+        fun subtractDayFromCurrentTime(amount: Int, currentDateString: String): String {
+            val sdf = SimpleDateFormat("dd-MM-yyyy")
+            val dateFromCurrent: Date = sdf.parse(currentDateString)
+            val cal = GregorianCalendar()
+            cal.time = dateFromCurrent
+            cal.add(Calendar.DATE, -amount)
+            return sdf.format(cal.time)
+        }
+    }
+}

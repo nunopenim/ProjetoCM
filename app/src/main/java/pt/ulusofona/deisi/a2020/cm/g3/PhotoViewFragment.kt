53e665d2ff04beb179cfa0146f1d1a96389ab091
nunopenim <nuno.penim@gmail.com>
2021-06-08 17:17:43 +0100

    PhotoViewFragment: Cleanup of unused imports

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/PhotoViewFragment.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/PhotoViewFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProviders
 import com.github.chrisbanes.photoview.PhotoView
 import pt.ulusofona.deisi.a2020.cm.g3.viewmodel.TestDetailViewModel
 

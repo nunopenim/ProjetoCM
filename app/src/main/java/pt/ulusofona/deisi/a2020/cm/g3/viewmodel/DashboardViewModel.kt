af054461a9220048e3766c8b60b30ffe5a195cdb
nunopenim <nuno.penim@gmail.com>
2021-06-08 10:00:49 +0100

    DashboardViewModel: threading and data recieving, you know

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/viewmodel/DashboardViewModel.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/viewmodel/DashboardViewModel.kt
@@ -2,17 +2,49 @@ package pt.ulusofona.deisi.a2020.cm.g3.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.github.mikephil.charting.data.BarData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import pt.ulusofona.deisi.a2020.cm.g3.blocs.API.Data
+import pt.ulusofona.deisi.a2020.cm.g3.interfaces.OnDataRecieved
 import pt.ulusofona.deisi.a2020.cm.g3.logic.DashboardLogic
+import pt.ulusofona.deisi.a2020.cm.g3.remote.DataObtainer
 
 class DashboardViewModel : ViewModel() {
     private val logic: DashboardLogic = DashboardLogic()
+    private var listener: OnDataRecieved? = null
+    var data = Data("", "",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+    var data_IO = Data("", "",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
 
     fun onLoadCardBuilder() : Array<String> {
-        return arrayOf(logic.getConfirmedStr(), logic.getRecoveredStr(), logic.getInternatedStr(), logic.getDeathsStr())
+        return arrayOf(logic.getConfirmedStr(data), logic.getRecoveredStr(data), logic.getInternatedStr(data), logic.getDeathsStr(data))
     }
 
     fun onLoadGraphBuilder() : BarData {
-        return logic.buildBarChart()
+        return logic.buildBarChart(data)
+    }
+
+    private fun notifyOnDataRecieved() {
+        listener?.onDataRecieved(data)
+    }
+
+    fun registerListener(listener: OnDataRecieved) {
+        this.listener = listener
+        listener?.onDataRecieved(data)
+    }
+
+    fun unregisterListener() {
+        listener = null
+    }
+
+    fun onStartDashboard() {
+        CoroutineScope(Dispatchers.IO).launch {
+            data_IO = DataObtainer.getData()
+            CoroutineScope(Dispatchers.Main).launch {
+                data = data_IO
+                notifyOnDataRecieved()
+            }
+        }
     }
 
 }

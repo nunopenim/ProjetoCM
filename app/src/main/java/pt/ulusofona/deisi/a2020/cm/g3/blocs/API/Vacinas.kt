7f73f0ce97d31d58a1835b446654368a8d941dac
nunopenim <nuno.penim@gmail.com>
2021-06-09 16:23:44 +0100

    Vacinas: added a function to convert to it's comrade that resides in a DB

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/blocs/API/Vacinas.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/blocs/API/Vacinas.kt
@@ -1,7 +1,11 @@
 package pt.ulusofona.deisi.a2020.cm.g3.blocs.API
 
+import pt.ulusofona.deisi.a2020.cm.g3.data.entities.VacinaDb
+
 class Vacinas (var data: String, var doses: Int?, var doses_novas: Int?,
                var doses1: Int?, var doses1_novas: Int?, var doses2: Int?, var doses2_novas: Int?) {
 
-    //falta métodos
+    fun convertToVacinaDb() : VacinaDb {
+        return VacinaDb(data, doses, doses_novas,doses1, doses1_novas, doses2, doses2_novas)
+    }
 }

497c47a6aa36c47e7548fecbdfc7feb8e2eefc06
nunopenim <nuno.penim@gmail.com>
2021-06-09 16:47:05 +0100

    VacinaDb: fixed missing entity tag

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/data/entities/VacinaDb.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/data/entities/VacinaDb.kt
@@ -1,13 +1,14 @@
 package pt.ulusofona.deisi.a2020.cm.g3.data.entities
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 import pt.ulusofona.deisi.a2020.cm.g3.blocs.API.Vacinas
 
+@Entity
 data class VacinaDb(var data: String, var doses: Int?, var doses_novas: Int?,
                     var doses1: Int?, var doses1_novas: Int?, var doses2: Int?, var doses2_novas: Int?) {
 
-    @PrimaryKey
-    var uuid = "1" // It's a surprise too that will help us with SQL :)
+    @PrimaryKey var uuid = "1" // It's a surprise too that will help us with SQL :)
 
     fun convertToVacinasObject() : Vacinas{
         return Vacinas(data, doses, doses_novas, doses1, doses1_novas, doses2, doses2_novas)

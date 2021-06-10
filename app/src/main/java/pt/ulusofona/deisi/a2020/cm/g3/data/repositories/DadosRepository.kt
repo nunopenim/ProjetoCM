d3d28227cbd70440a1f25ce92a24e442e438d412
nunopenim <nuno.penim@gmail.com>
2021-06-10 16:44:30 +0100

    DadosRepository: we buffer the data while the API is not updated now

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/data/repositories/DadosRepository.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/data/repositories/DadosRepository.kt
@@ -36,6 +36,29 @@ class DadosRepository(private val local: DataDao) {
             try {
                 IO_data = DataObtainer.getData()
                 ableToLoad = true
+                if(IO_data.confirmados_0_9f == 0) {
+                    val obj = local.getLatest("1").convertToDataObject()
+                    if (obj != null) {
+                        IO_data.confirmados_0_9f = obj.confirmados_0_9f
+                        IO_data.confirmados_0_9m = obj.confirmados_0_9m
+                        IO_data.confirmados_10_19f = obj.confirmados_10_19f
+                        IO_data.confirmados_10_19m = obj.confirmados_10_19m
+                        IO_data.confirmados_20_29f = obj.confirmados_20_29f
+                        IO_data.confirmados_20_29m = obj.confirmados_20_29m
+                        IO_data.confirmados_30_39f = obj.confirmados_30_39f
+                        IO_data.confirmados_30_39m = obj.confirmados_30_39m
+                        IO_data.confirmados_40_49f = obj.confirmados_40_49f
+                        IO_data.confirmados_40_49m = obj.confirmados_40_49m
+                        IO_data.confirmados_50_59f = obj.confirmados_50_59f
+                        IO_data.confirmados_50_59m = obj.confirmados_50_59m
+                        IO_data.confirmados_60_69f = obj.confirmados_60_69f
+                        IO_data.confirmados_60_69m = obj.confirmados_60_69m
+                        IO_data.confirmados_70_79f = obj.confirmados_70_79f
+                        IO_data.confirmados_70_79m = obj.confirmados_70_79m
+                        IO_data.confirmados_80f = obj.confirmados_80f
+                        IO_data.confirmados_80m = obj.confirmados_80m
+                    }
+                }
             } catch(e: Exception) {
                 val obj = local.getLatest("1")
                 if (obj != null) {

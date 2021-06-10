503aad2f95f3962e9c0d9e653220557f94441ba6
nunopenim <nuno.penim@gmail.com>
2021-06-10 16:31:37 +0100

    general: Battery is done, project is finished

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/MainActivity.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/data/battery/Battery.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/data/battery/Battery.kt
@@ -1,13 +1,21 @@
 package pt.ulusofona.deisi.a2020.cm.g3.data.battery
 
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.res.Configuration
 import android.os.BatteryManager
 import android.os.Handler
 import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
+
 class Battery private constructor(private val context: Context) : Runnable{
 
     private val TAG = Battery::class.java.simpleName
-    private val TIME_BETWEEN_UPDATES = 20 * 1000L
+    private val TIME_BETWEEN_UPDATES = 15 * 1000L
+
+    private var hasStarted = false
+    private var isDark = false
 
     companion object{
         private var instance: Battery? = null
@@ -27,9 +35,41 @@ class Battery private constructor(private val context: Context) : Runnable{
         return manager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
     }
 
+    //De acordo com o Piazza, podemos usar isto: https://piazza.com/class/kl2dr8a92j2a5?cid=95
+    //https://developer.android.com/training/monitoring-device-state/battery-monitoring
+    private fun batteryLevel() : Float{
+        val batteryStatus: Intent? = IntentFilter(Intent.ACTION_BATTERY_CHANGED).let { ifilter ->
+            context.registerReceiver(null, ifilter)
+        }
+        val batteryPct: Float? = batteryStatus?.let { intent ->
+            val level: Int = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val scale: Int = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+            level * 100 / scale.toFloat()
+        }
+        return batteryPct!!
+    }
+
     override fun run() {
-        val current = getBaterryCurrentNow()
-        Log.i(TAG, current.toString())
+        if (!hasStarted) {
+            val nightModeFlags = context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            when (nightModeFlags) {
+                Configuration.UI_MODE_NIGHT_YES -> isDark = true
+                Configuration.UI_MODE_NIGHT_NO -> isDark = false
+                Configuration.UI_MODE_NIGHT_UNDEFINED -> isDark = false
+            }
+            hasStarted = true
+        }
+
+        val current = batteryLevel()
+        if(!isDark) {
+            if(current < 20) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+        Log.i("BatteryLevel", current.toString())
         handler.postDelayed(this,TIME_BETWEEN_UPDATES)
     }
 }

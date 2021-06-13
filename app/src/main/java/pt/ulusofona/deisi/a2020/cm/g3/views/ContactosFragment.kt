779a7f279dccfdc8e8308d019339aac9d992bd51
nunopenim <nuno.penim@gmail.com>
2021-06-13 08:58:30 +0100

    ContactosFragment: cleanup of unused imports

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/views/ContactosFragment.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g3/views/ContactosFragment.kt
@@ -4,8 +4,6 @@ import android.Manifest
 import android.location.Address
 import android.location.Geocoder
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
